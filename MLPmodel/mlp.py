ad08e8e7132debf34858390b01d2f9adafdfd0b2
Jacob Redden <jredden@jacob-desktop.lan>
2022-11-18 18:37:03 -0600

    I should've commited a long time ago...

---
+++ MLPmodel/mlp.py
@@ -0,0 +1,97 @@
+import random
+import numpy as np
+from tqdm import trange
+from sklearn.model_selection import StratifiedKFold
+from MLPmodel.layers import *
+
+class MLP:
+    def __init__(self, inputNodes, hiddenLayers, hiddenLayerNodes, outputNodes, learningRate, epochs, Kfolds):
+        self.inputNodes = inputNodes
+        self.hiddenLayers = hiddenLayers
+        self.hiddenLayerNodes = hiddenLayerNodes
+        self.outputNodes = outputNodes
+        self.learningRate = learningRate
+        self.epochs = epochs
+        self.Kfolds = Kfolds
+    
+        self.network = []
+        self.train_log = []
+        self.val_log = []
+
+        self.setUpNetwork()
+
+    def setUpNetwork(self):
+        self.network.append(Dense(self.inputNodes, self.hiddenLayerNodes))
+        for _ in range(self.hiddenLayers-1):
+            self.network.append(ReLU())
+            self.network.append(Dense(self.hiddenLayerNodes, self.hiddenLayerNodes))
+        self.network.append(ReLU())
+        self.network.append(Dense(self.hiddenLayerNodes, self.outputNodes))
+
+    def softmax_crossentropy_with_logits(self, logits, reference_answers):
+        logits_for_answers = logits[np.arange(len(logits)), reference_answers]
+
+        xentropy = - logits_for_answers + np.log(np.sum(np.exp(logits),axis=1))
+        return xentropy
+
+    def grad_softmax_crossentropy_with_logits(self, logits, reference_answers):
+        ones_for_answers = np.zeros_like(logits)
+        ones_for_answers[np.arange(len(logits)),reference_answers] = 1
+
+        softmax = np.exp(logits) / np.exp(logits).sum(axis=-1,keepdims=True)
+        return (- ones_for_answers+softmax) / logits.shape[0]
+
+    def forward(self, X):
+        activations = []
+        layerInput = X
+
+        for l in self.network:
+            activations.append(l.forward(layerInput))
+            layerInput = activations[-1]
+
+        assert len(activations) == len(self.network)
+        return activations
+
+    def iterate_minibatches(self, inputs, targets, batchsize, shuffle=False):
+        assert len(inputs) == len(targets)
+        if shuffle:
+            indices = np.random.permutation(len(inputs))
+        for start_idx in trange(0, len(inputs) - batchsize + 1, batchsize):
+            if shuffle:
+                excerpt = indices[start_idx:start_idx + batchsize]
+            else:
+                excerpt = slice(start_idx, start_idx + batchsize)
+            yield inputs[excerpt], targets[excerpt]
+
+    def predict(self, X):
+        logits = self.forward(X)[-1]
+        print(logits.argmax(axis=-1))
+        return logits.argmax(axis=-1)
+        
+    def train(self, X, y):
+        X = np.array(X)
+        y = np.array(y)
+        kf = StratifiedKFold(n_splits=self.Kfolds)
+        for epoch in range(self.epochs):
+            for train_index, val_index in kf.split(X, y):
+                X_train, X_val = X[train_index,:], X[val_index,:]
+                y_train, y_val = y[train_index], y[val_index]
+                for x_batch, y_batch in self.iterate_minibatches(X_train, y_train, 20, False):
+                    layer_activations = self.forward(x_batch)
+                    layer_inputs = [x_batch]+layer_activations
+                    logits = layer_activations[-1]
+
+                    # loss = self.softmax_crossentropy_with_logits(logits, y_batch)
+                    loss_grad = self.grad_softmax_crossentropy_with_logits(logits, y_batch)
+                    
+                    for layer_index in range(len(self.network))[::-1]:
+                        layer = self.network[layer_index]
+                        loss_grad = layer.backward(layer_inputs[layer_index], loss_grad)
+            
+            self.train_log.append(np.mean(self.predict(X_train)==y_train))
+            print(y_train)
+            self.val_log.append(np.mean(self.predict(X_val)==y_val))
+
+            print('epoch: %d/%d' % (epoch+1, self.epochs))
+            print('\ttrain accuracy =      %.03f%%' % self.train_log[-1])
+            print('\tvalidation accuracy = %.03f%%' % self.val_log[-1])
---
+++ genreClassificationDriver.py
@@ -0,0 +1,115 @@
+import sys
+import os
+import argparse
+import json
+import numpy as np
+from sklearn.model_selection import train_test_split
+from MLPmodel.mlp import MLP
+
+NUMCHROMAGRAMSAMPLES = 1000
+
+def getArgParser():
+    desc =  'This driver runs a genre classification Multi Level Perceptron neural netwrok\n\n'
+    desc += 'A preprocessiong of mp3 files to json files is required' 
+    # TODO add more
+
+    parser = argparse.ArgumentParser(
+        prog = 'genreClassificationDriver: MLP genre classification',
+        description = desc,
+        epilog = "" # TODO
+    )
+    parser.add_argument('data', type=str, help='folder containing database of json files')
+
+    return parser
+
+def getJsonFiles(dataFolder):
+    root = os.getcwd()
+    path = os.path.join(root, dataFolder)
+
+    jsonFiles = []
+    for r, d, f in os.walk(path):
+        for file in f:
+            if file.endswith('.json'):
+                jsonFiles.append(os.path.join(r, file))
+
+    return jsonFiles
+
+def extractData(jsonFiles):
+    X = []
+    y = []
+    genreMap = {}
+    xEntry = np.zeros(1 + 1 + NUMCHROMAGRAMSAMPLES*12 + NUMCHROMAGRAMSAMPLES + NUMCHROMAGRAMSAMPLES + NUMCHROMAGRAMSAMPLES*6 + NUMCHROMAGRAMSAMPLES + NUMCHROMAGRAMSAMPLES)
+    for jsonFile in jsonFiles:
+        print('Extracting data for %s' % jsonFile)
+        f = open(jsonFile)
+        jsonData = json.load(f)
+        idx = 0
+        # tempo
+        xEntry[idx] = jsonData['tempo']/1000
+        idx += 1
+        # tuning
+        xEntry[idx] = jsonData['tuning']/10000
+        idx += 1
+        # chromagram samples
+        numSamples = len(jsonData['chromagram'][0])
+        for i in range(12):
+            for counter in range(NUMCHROMAGRAMSAMPLES):
+                jsonIdx = int(np.floor((counter/(NUMCHROMAGRAMSAMPLES+1))*numSamples))
+                xEntry[idx] = jsonData['chromagram'][i][jsonIdx]
+                idx += 1
+
+        # spec_bw samples 
+        numSamples = len(jsonData['spec_bw'][0])
+        maxSample = np.max(jsonData['spec_bw'])
+        for counter in range(NUMCHROMAGRAMSAMPLES):
+            jsonIdx = int(np.floor((counter/(NUMCHROMAGRAMSAMPLES+1))*numSamples))
+            xEntry[idx] = jsonData['spec_bw'][0][jsonIdx]/maxSample
+            idx += 1
+        # constrast samples 6 bands
+        numSamples = len(jsonData['contrast'][0])
+        maxSample = np.max(jsonData['contrast'])
+        for i in range(6):
+            for counter in range(NUMCHROMAGRAMSAMPLES):
+                jsonIdx = int(np.floor((counter/(NUMCHROMAGRAMSAMPLES+1))*numSamples))
+                xEntry[idx] = jsonData['contrast'][i][jsonIdx]/maxSample
+                idx += 1
+
+        # maxRolloff samples
+        numSamples = len(jsonData['maxRolloff'][0])
+        maxSample = np.max(jsonData['maxRolloff'])
+        for counter in range(NUMCHROMAGRAMSAMPLES):
+            jsonIdx = int(np.floor((counter/(NUMCHROMAGRAMSAMPLES+1))*numSamples))
+            xEntry[idx] = jsonData['maxRolloff'][0][jsonIdx]/maxSample
+            idx += 1
+        # minRolloff samples
+        numSamples = len(jsonData['minRolloff'][0])
+        maxSample = np.max(jsonData['minRolloff'])
+        for counter in range(NUMCHROMAGRAMSAMPLES):
+            jsonIdx = int(np.floor((counter/(NUMCHROMAGRAMSAMPLES+1))*numSamples))
+            xEntry[idx] = jsonData['minRolloff'][0][jsonIdx]/maxSample
+            idx += 1
+        X.append(xEntry)
+        if jsonData['genre'] not in genreMap:
+            genreMap[jsonData['genre']] = len(genreMap)
+        y.append(genreMap[jsonData['genre']])
+
+    return X, y, genreMap
+
+def main(args):
+    dataFolder = args.data
+    jsonFilenames = getJsonFiles(dataFolder)
+    X, y, genreMap = extractData(jsonFilenames)
+
+    print(len(X))
+    print(len(y))
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    model = MLP(len(X_train[0]), 1000, 1000, len(genreMap), 100, 100, 5)
+    model.train(X_train, y_train)
+
+
+if __name__ == '__main__':
+    parser = getArgParser()
+    args = parser.parse_args()
+    main(args)
