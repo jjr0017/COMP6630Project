a76f9974c2653ce3ad78f004cfaa02a6aa0409a1
Alex Cole <jac0038@uah.edu>
2022-12-06 19:59:26 +0000

    Pep-8

--- database/createDatabaseFromScratch.py
+++ database/createDatabaseFromScratch.py
@@ -7,15 +7,15 @@ import database.featureExtraction
 def main():
     folder = './'
     if len(sys.argv) > 1:
-        folder = sys.argv[1]
+        folder: str = sys.argv[1]
     if len(sys.argv) > 2:
-        maxFiles = int(sys.argv[2])
+        maxFiles: int = int(sys.argv[2])
     
     if not os.path.isdir(folder):
         print(folder + ' not a folder')
         exit(1)
     
-    database.createMetadata.parseWebsiteForMetadata('https://freemusicarchive.org/', folder)
+    database.createMetadata.parse_website_for_metadata('https://freemusicarchive.org/', folder)
     database.downloadMP3s.downloadMP3s(folder, maxFiles=maxFiles)
     database.featureExtraction.featureExtraction(folder)
 
--- database/createMetadata.py
+++ database/createMetadata.py
@@ -6,115 +6,114 @@ import os
 
 numSongsPerGenre = 1
 
-GENRES = [  # 'INTERNATIONAL',
-            # 'BLUES',
-            'JAZZ',
-            # 'NOVELTY',
-            # 'HISTORIC',
-            'COUNTRY',
-            'POP',
-            'ROCK',
-            'SOUL-RNB',
-            # 'SPOKEN',
-            # 'EXPERIMENTAL',
-            # 'FOLK',
-            # 'INSTRUMENTAL',
-            'CLASSICAL',
-            # 'ELECTRONIC',
-            'HIP-HOP'
-            ]
+GENRES: List[str] = [
+    'JAZZ',
+    'COUNTRY',
+    'POP',
+    'ROCK',
+    'SOUL-RNB',
+    'CLASSICAL',
+    'HIP-HOP'
+]
 
-def storeDataTrackInfo(genre, dataTrackJson):
-    j = json.loads(dataTrackJson)
+
+def store_data_track_info(genre, data_track_json) -> str:
+    j = json.loads(data_track_json)
+    
     id = j["data-track-info"]["id"]
     handle = j["data-track-info"]["handle"]
     url = j["data-track-info"]["url"]
     title = j["data-track-info"]["title"]
-    artistName = j["data-track-info"]["artistName"]
-    artistUrl = j["data-track-info"]["artistUrl"]
-    albumTitle = j["data-track-info"]["albumTitle"]
-    playbackUrl = j["data-track-info"]["playbackUrl"]
-    downloadUrl = j["data-track-info"]["downloadUrl"]
-    fileName = j["data-track-info"]["fileName"]
-    fileUrl = j["data-track-info"]["fileUrl"]
+    artist_name = j["data-track-info"]["artistName"]
+    artist_url = j["data-track-info"]["artistUrl"]
+    album_title = j["data-track-info"]["albumTitle"]
+    play_back_url = j["data-track-info"]["playbackUrl"]
+    download_url = j["data-track-info"]["downloadUrl"]
+    file_name = j["data-track-info"]["fileName"]
+    file_url = j["data-track-info"]["fileUrl"]
 
-    c = FreeMusicArchive(genre, id, handle, url, title, artistName, artistUrl, albumTitle, playbackUrl, downloadUrl, fileName, fileUrl)
+    c = FreeMusicArchive(genre, id, handle, url, title, artist_name,
+                         artist_url, album_title, play_back_url, download_url, file_name, file_url)
 
     return json.dumps(c.__dict__)
 
 
-def getDataTrackInfo(genre, html):
-    ret = False
-    lines = html.splitlines()
-    j = ''
+def get_data_track_info(genre, html):
+    ret: bool = False
+    lines: List[str] = html.splitlines()
+    j: str = ''
     for line in lines:
         if 'data-track-info' in line:
-            ret = True
+            ret: bool = True
             # print(line)
 
-            startIdx = line.find('data-track-info') + len('data-track-info') + 2
-            endIdx = line.rfind("}") + 1
-            info = line[startIdx:endIdx].replace('\\', '')
-            dataTrackJson = '{"data-track-info": %s}' % info
-            j += '\t\t' + storeDataTrackInfo(genre, dataTrackJson) + ',\n'
+            start_idx = line.find('data-track-info') + \
+                len('data-track-info') + 2
+            end_index = line.rfind("}") + 1
+            info = line[start_idx:end_index].replace('\\', '')
+            data_track_json = '{"data-track-info": %s}' % info
+            j += '\t\t' + store_data_track_info(genre, data_track_json) + ',\n'
 
     return ret, j
 
-def genreHtmlPagesToJson(genre, url, f):
-    pageCounter = 1
-    fullJson = ''
+
+def genre_html_pages_to_json(genre, url, f) -> None:
+    page_counter: int = 1
+    full_json: str = ''
     while 1:
-        print('page: %d' % pageCounter)
-        response = requests.get(url+str(pageCounter))
+        print('page: %d' % page_counter)
+        response = requests.get(url+str(page_counter))
         if response.ok:
             contents = response.text
-            foundSongs, j = getDataTrackInfo(genre, contents)
-            if foundSongs:
-                pageCounter += 1
-                fullJson += j
+            found_songs, j = get_data_track_info(genre, contents)
+            if found_songs:
+                page_counter += 1
+                full_json += j
             else:
                 break
         else:
             break
-    f.write(fullJson[:-2])
-    return
+    f.write(full_json[:-2])
 
-def parseWebsiteForMetadata(url, folder):
+
+def parse_website_for_metadata(url, folder) -> None:
     folder += '/data/'
     if not os.path.isdir(folder):
         os.mkdir(folder)
 
     f = open(folder + 'mp3Metadata.json', 'w')
     f.write('{ "songMetadata": \n\t[\n')
-    metaDataJson = ''
+
     for genre in GENRES:
         print(genre)
-        genreUrl = url + 'genre/' + genre.title() + '?sort=date&d=0&pageSize=200&page='
+        genre_url: str = url + 'genre/' + \
+            genre.title() + '?sort=date&d=0&pageSize=200&page='
 
         # fix novelty
-        if 'Novelty' in genreUrl:
-            genreUrl = genreUrl.replace('Novelty', 'novelty')
-        if 'Historic' in genreUrl:
-            genreUrl = genreUrl.replace('Historic', 'Old-Time__Historic')
-        if 'Soul-Rnb' in genreUrl:
-            genreUrl = genreUrl.replace('Soul-Rnb', 'Soul-RB')
-        if not genre == GENRES[0]:
+        if 'Novelty' in genre_url:
+            genre_url: str = genre_url.replace('Novelty', 'novelty')
+        if 'Historic' in genre_url:
+            genre_url = genre_url.replace('Historic', 'Old-Time__Historic')
+        if 'Soul-Rnb' in genre_url:
+            genre_url = genre_url.replace('Soul-Rnb', 'Soul-RB')
+        if genre != GENRES[0]:
             f.write(',\n')
-        genreHtmlPagesToJson(genre, genreUrl, f)
+        genre_html_pages_to_json(genre, genre_url, f)
 
     f.write('\n\t]\n}')
     f.close()
 
-def main():
-    folder = './'
+
+def main() -> None:
+    folder: str = './'
     if len(sys.argv) > 1:
         folder = sys.argv[1]
     if not os.path.isdir(folder):
         print(folder + ' not a folder')
         exit(1)
 
-    parseWebsiteForMetadata('https://freemusicarchive.org/', folder)
-    return
+    parse_website_for_metadata('https://freemusicarchive.org/', folder)
+
 
 if __name__ == '__main__':
     main()
